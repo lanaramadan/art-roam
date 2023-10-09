6d40289712ce495355289b6088f38d43bced94a5
Lana Ramadan <lanamramadan@gmail.com>
2023-10-09 01:34:32 -0700

    Adding css to the the main image display

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
   const [image, setImage] = useState("");
   const [museumJsonData, setMuseumJsonData] = useState(null);
-  const [imageLink, setImageLink] = useState(null);
+  const [currentImageData, setCurrentImageData] = useState(null);
 
   const getImage = () => {
     let query = `${baseMuseumApiUrl}`;
@@ -18,22 +18,61 @@ const App = () => {
       const response = await fetch(query);
       const retreivedJson = await response.json();
       setMuseumJsonData(retreivedJson)
-      
+
+      setCurrentImageData(museumJsonData.data[3])
+
       // gets the image link
-      const imageLink = `${baseImageApiUrl}/${museumJsonData.data[3].image_id}/full/843,/0/default.jpg`
+      const imageLink = `${baseImageApiUrl}/${currentImageData.image_id}/full/843,/0/default.jpg`
       setImage(imageLink);
     }
 
     callMuseumAPI(query)
   };
 
-
   return (
     <div className="App">
-      <img src= {image} />
-      <button type="button" className="button submit" onClick={() => getImage()}>
-        Click me!
-      </button>
+
+      <div className="image-display">
+
+        <div className="header">
+          <h1 className='title'>Art Roam 🖼️</h1>
+          <h2 className='slogan'>Discover Art, One Click at a Time!</h2>
+        </div>
+
+
+        <div className="art-description">
+          <h2 className='art-title'>{currentImageData.title}</h2>
+          <div className='characteristics'>
+            <button type="button" className="button characteristic" onClick={() => getImage()}>
+              Artist: {currentImageData.artist_title}
+            </button>
+
+            <button type="button" className="button characteristic" onClick={() => getImage()}>
+              Dates: {currentImageData.date_start}-{currentImageData.date_end}
+            </button>
+
+            <button type="button" className="button characteristic" onClick={() => getImage()}>
+              Country: {currentImageData.place_of_origin}
+            </button>
+
+            <button type="button" className="button characteristic" onClick={() => getImage()}>
+              Medium: {currentImageData.medium_display}
+            </button>
+          </div>
+        </div>
+
+
+        <div className="image-container">
+          <div className="frame"><img src={image} className='main-image'/></div>
+
+          <button type="button" className="button generate" onClick={() => getImage()}>
+            🔀 New Art!
+          </button>
+        </div>
+
+
+      </div>
+     
     </div>
   )
 }
